b637c29dfa4e860fceee034bd6ab0be59589fddf
taka <84855824+Takase0917@users.noreply.github.com>
2023-04-07 00:22:25 +0100

    Delete Desktop/tutorial/client directory

--- Desktop/tutorial/client/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Login from './Login';
-
-const root = document.getElementById('root');
-createRoot(root).render(<Login />);
-
